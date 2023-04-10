3f0268535f927ee48a432076580f2111c917d8f6
HypeDeveloper <de.hypedeveloper@gmail.com>
2023-04-10 01:03:45 +0100

    update

--- frontend/src/pages/umuasa.jsx
+++ frontend/src/pages/umuasa.jsx
@@ -7,17 +7,7 @@ export default function AboutMe() {
     return (
         <>
             <section id="umu-about">
-                <div className="mobileImg">
-                <img src={hero1img} alt="" />
-
-                </div>
-                <div className="aboutImg1">
-                    <img src={hero1img} alt="" />
-                </div>
-                <div className="aboutImg2">
-                    <img src={hero2img} alt="" />
-
-                </div>
+                
                 <div className="umu-wrap">
 
                     <div className="text-box">
@@ -26,45 +16,6 @@ export default function AboutMe() {
                             Energy
                             Queen
                         </h1>
-                        <p className="text">
-                            I’m a Lawyer with a Masters degree in Oil & Gas law. I am a Hotelier & I’m the Managing director of Sefcon group of hotels, a Brand Ambassador , an Influencer  and a YouTuber.
-                        </p>
-                    </div>
-                    {/* <div className="img2-about"></div> */}
-                </div>
-            </section>
-
-            <section id="umu-SlideShow">
-                <div className="wrap-umu">
-                    <div className="slideInfo">
-                        <h1 className="headerMain .pad-b-2 text-width-300">
-                            What
-                            Popping’
-                        </h1>
-                        <br />
-                        <p className="text">
-                            happily married to the most supportive man and we have 2 beautiful energetic kids together who mean the whole world to me . One is an actual celebrity lol
-                        </p>
-                    </div>
-                </div>
-            </section>
-
-
-            <section id="frameHall-about">
-                <div className="info-wrap-hall">
-                    <div className="info-wrap">
-                        <h1 className="headerMain text-width-500">
-                            My Hall <br />
-                            of Fame
-                        </h1> <br />
-                        <p className='text text-width-500'>
-                            {/* info */}
-                        </p>
-                    </div>
-                    <div className="info-vid">
-                        <div className="videoMashup">
-                            <iframe src="https://www.youtube.com/embed/u7amIzuw_YU" frameBorder="0"></iframe>
-                        </div>
                     </div>
                 </div>
             </section>
